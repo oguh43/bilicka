31f490d174c9bc613bc12e05169a528ae528a55d
Hugo Bohácsek <agenthugo43@gmail.com>
2019-12-05 18:49:48 +0100

    yay

--- Krasokorčuľovanie v2.py
+++ Krasokorčuľovanie v2.py
@@ -1,7 +1,5 @@
 ls=[]
-for i in range(0,10):
-  ls.append(int(input(str(i+1) +". Skore:")))
+for i in range(0,10): ls.append(int(input(str(i+1) +". Skore:")))
 ls.sort(reverse=True)
-ls.pop(0)
-ls.pop(8)
+ls.pop(0) and ls.pop(8)
 print("Vysledne skore je: " +str(sum(ls)/float(len(ls))))
--- ulohy_2.py
+++ ulohy_2.py
@@ -1,5 +1,20 @@
-print("y")
-if "1"=="1":
-    print("1")
-else:
-    print("2")
+def pankrac():
+    y = str(input("Veta? ")).split(" ")
+    ls = []
+    ls1 = []
+    nn = 0
+    for i in range(0, len(y)):
+        if y[i].istitle() == True: ls.append(y[i].swapcase())
+        elif y[i].isupper() == True:
+            sp = y[i] #make list()
+            sp = list(sp)
+            for n in sp: #remove nn
+                if nn == 0:
+                    ls1.append(n.swapcase())
+                    nn = nn + 1
+                else: ls1.append(n)
+            ls.append("".join(ls1))
+        else: ls.append(y[i])
+    print(" ".join(ls))
+
+pankrac()
