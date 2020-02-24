d8133cabdb2659e5281518160bce9a7e3840f46e
Hugo Bohácsek <agenthugo43@gmail.com>
2020-02-24 15:58:29 +0100

    beta

--- tabulky.py
+++ tabulky.py
@@ -1,13 +1,12 @@
-import math
-import sys
-import time
+from datetime import date
 
 main_value_list = []
 first_column_list = []
 second_column_list = []
+third_column_list = []
 results_list = [""]
-
-
+relative = None
+interval = []
 
 
 while True:
@@ -17,18 +16,33 @@ while True:
     else:
         break
 
-print(main_value_list)
 
 
 number_of_values = len(main_value_list)
+
 for i in range(1,number_of_values+1):
     first_column_list.append(i)
 
 results_list.append(sum(main_value_list)/len(main_value_list))
-print(results_list)
-print(first_column_list)
 
 for i in range(len(main_value_list)):
     second_column_list.append(main_value_list[i]-results_list[1])
-print(second_column_list)
 
+results_list.append(0)
+
+for i in range(len(second_column_list)):
+    third_column_list.append(abs(second_column_list[i]))
+
+results_list.append(sum(third_column_list)/len(second_column_list))
+relative = (results_list[3]/results_list[1])*100
+interval.append(results_list[1]-relative)
+interval.append(results_list[1]+relative)
+
+print("+----+---------+---------+---------+")
+for i in range(len(first_column_list)):
+    print("|",first_column_list[i]," "*(2-len(str(first_column_list[i])))+"|",round(main_value_list[i],2)," "*(7-len(str(round(main_value_list[i],2))))+"|",round(second_column_list[i],2)," "*(7-len(str(round(second_column_list[i],2))))+"|",round(third_column_list[i],2)," "*(7-len(str(round(third_column_list[i],2))))+"|")
+    print("+----+---------+---------+---------+")
+
+print("Relatívna odchýlka: ",relative,"%",sep="")
+print("Najpravdepodobnejšia hodnota je: (",interval[0],";",interval[1],")",sep="")
+print(date.today())
