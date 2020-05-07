6490d2a781c2c7b1193995b23a4737a30ce41d43
Hugo Bohácsek <agenthugo43@gmail.com>
2020-05-07 14:59:41 +0200

    Merged files containing excercises from single source

--- vieme_to.py
+++ vieme_to.py
@@ -1,3 +1,11 @@
+def cross(n):
+    for i in range(n):
+        print("."*n,"#"*n,"."*n, sep = "")
+    for i in range(n):
+        print("#"*(n*3))
+    for i in range(n):
+        print("."*n,"#"*n,"."*n, sep = "")
+
 def big_frame(word_list):
     m = 0
     for i in word_list:
@@ -7,5 +15,6 @@ def big_frame(word_list):
     for i in range(len(word_list)):
         print("+",word_list[i]," "*(m-len(word_list[i])),"+", sep = "")
     print("+"*(m+2))
-    
+
+cross(3)    
 big_frame(["Wellcome", "to", "Brno"])
