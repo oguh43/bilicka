130757f302c06870a95c5618ecb9257d44dd7f53
Hugo Bohácsek <agenthugo43@gmail.com>
2020-05-07 15:13:05 +0200

    Cleanup

--- Krasokorčuľovanie v2.py
+++ Krasokorčuľovanie v2.py
@@ -1,5 +1,5 @@
 ls=[]
 for i in range(0,10): ls.append(int(input(str(i+1) +". Skore:")))
 ls.sort(reverse=True)
-ls.pop(0) and ls.pop(8)
-print("Vysledne skore je: " +str(sum(ls)/float(len(ls))))
+ls.pop(0); ls.pop(-1)
+print("Vysledne skore je: " +str(sum(ls)/(len(ls))))
