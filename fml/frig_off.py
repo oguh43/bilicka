d01266c63b4e5b0c26fbab6190eaec67588e91e4
oguh43 <agenthugo43@gmail.com>
2022-01-09 19:58:52 +0100

    bypass plagiarism checkers

---
+++ fml/frig_off.py
@@ -0,0 +1,18 @@
+from random import randint
+
+def insert(source_str, insert_str, pos):
+    return source_str[:pos] + insert_str + source_str[pos:]
+
+with open("manipulate.txt", "r") as f:
+    content = f.read()
+
+content = content.split(" ")
+
+for i in range(0, len(content)):
+    if len(content[i]) > 3:
+        content[i] = insert(content[i], "​", randint(1, len(content[i]) - 1))
+        
+content = " ".join(content)
+
+with open("out.txt", "w+", encoding="utf-8") as f:
+    f.write(content)
