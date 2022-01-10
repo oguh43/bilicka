edb650367bed40c5510aa728f2ff3f656fcf2a52
oguh43 <agenthugo43@gmail.com>
2022-01-10 22:05:56 +0100

    a

--- fml/frig_off.py
+++ fml/frig_off.py
@@ -1,18 +1,35 @@
+from __future__ import annotations
 from random import randint
 
-def insert(source_str, insert_str, pos):
+def insert(source_str: str, insert_str: str, pos: int) -> str:
     return source_str[:pos] + insert_str + source_str[pos:]
 
-with open("manipulate.txt", "r", encoding="utf-8") as f:
-    content = f.read()
 
-content = content.split(" ")
+if input("(A)gressive || (S)ilent bypass?").lower() == "a":
 
-for i in range(0, len(content)):
-    if len(content[i]) > 3:
-        content[i] = insert(content[i], "​", randint(1, len(content[i]) - 1))
-        
-content = " ".join(content)
+    with open("manipulate.txt", "r", encoding="utf-8") as f:
+        content = f.read()
 
-with open("out.txt", "w+", encoding="utf-8") as f:
-    f.write(content)
+    content = content.split(" ")
+
+    for i in range(0, len(content)):
+        if len(content[i]) > 3:
+            content[i] = insert(content[i], "​", randint(1, len(content[i]) - 1))
+            
+    content = " ".join(content)
+
+    with open("out.txt", "w+", encoding="utf-8") as f:
+        f.write(content)
+
+else:
+    with open("manipulate.txt", "r", encoding="utf-8") as f:
+        content = f.read()
+
+    if input("Append invisible char to the (L)eft, or (R)ight?").lower() == "L":
+        char = "​ "
+    else:
+        char = " ​"
+    content = char.join(content.split(" "))
+
+    with open("out.txt", "w+", encoding="utf-8") as f:
+        f.write(content)
